efa854d21968581b3b6eb10a923143b90cba04f3
Tako <takogiorgobiani97@gmail.com>
2022-09-01 22:46:53 +0400

    converting and checking numbers

--- script.js
+++ script.js
@@ -269,3 +269,21 @@ console.log(Number.parseInt('e30'));
 
 console.log(Number.parseFloat('2.5rem'));
 console.log(Number.parseInt('2.5rem'));
+
+// console.log(parseFloat('2.5rem'));
+
+// Check if value is NaN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN(23 / 0));
+
+// Cheking if value is number
+console.log(Number.isFinite(20));
+console.log(Number.isFinite('20'));
+console.log(Number.isFinite(+'20X'));
+console.log(Number.isFinite(23 / 0));
+
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
