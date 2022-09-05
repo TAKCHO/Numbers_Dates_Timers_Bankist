4470eaa55c5545c6016b947305d429c1dbeaf1b2
Tako <takogiorgobiani97@gmail.com>
2022-09-05 23:07:39 +0400

    math and rounding 1

--- script.js
+++ script.js
@@ -297,3 +297,24 @@ console.log(Math.max(5, 4, 65, 3, 6));
 console.log(Math.max(5, 4, '65', 3, 6));
 
 console.log(Math.min(5, 4, 65, 3, 6));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) => Math.trunc(Math.random() * (max - min) + 1);
+// 0..1 -> 0 .. (max-min) - > min..max
+
+console.log(randomInt(10, 20));
+
+// Rounding integers
+console.log(Math.trunc(23.3));
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(33.3));
+console.log(Math.ceil(33.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
