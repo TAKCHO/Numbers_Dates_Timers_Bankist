5744d37f0bf3bfe4372ccde6cb5e348630d7238f
Tako <takogiorgobiani97@gmail.com>
2022-09-14 22:52:21 +0400

    Creating Dates pt 2

--- script.js
+++ script.js
@@ -411,7 +411,7 @@ console.log(10 / 3);
 ///// Creating Dates
 
 // Create a date
-const now = new Date();
+/* const now = new Date();
 console.log(now);
 
 console.log(new Date('2022-09-14T18:44:22.926Z'));
@@ -422,4 +422,25 @@ console.log(new Date(2037, 10, 19, 15, 23, 5));
 console.log(new Date(2037, 10, 31));
 
 console.log(new Date(0));
-console.log(new Date(3 * 24 * 60 * 60 * 1000));
+console.log(new Date(3 * 24 * 60 * 60 * 1000)); */
+
+// Working with dates
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142242580000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
