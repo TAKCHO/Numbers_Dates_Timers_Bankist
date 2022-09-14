d344776c3e1cec075e0c04c789df1d5cc42a833b
Tako <takogiorgobiani97@gmail.com>
2022-09-14 22:14:35 +0400

    Numeric Separators

--- script.js
+++ script.js
@@ -330,7 +330,7 @@ console.log(+(2.345).toFixed(2)); */
 
 ////// The remainder Operator
 
-console.log(5 % 2);
+/* console.log(5 % 2);
 console.log(5 / 2); // 5 = 2*2 +1
 
 console.log(8 % 3);
@@ -353,3 +353,22 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+ */
+
+/// Numeric Separators
+
+// 287,645,000,000
+const diameter = 287_645_000_000;
+console.log(diameter);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI);
+
+console.log(Number('230_000'));
+console.log(Number('230000'));
