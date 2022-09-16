e48ecb6fb04887077dbfe7d95a555f079a26d540
Tako <takogiorgobiani97@gmail.com>
2022-09-16 20:56:37 +0400

    operations with dates pt1

--- script.js
+++ script.js
@@ -481,3 +481,12 @@ console.log(Date.now());
 future.setFullYear(2040);
 console.log(future);
  */
+
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
