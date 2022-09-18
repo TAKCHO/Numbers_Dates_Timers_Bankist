91a1e57be989629acd1028d9d17f6287c63141c5
Tako <takogiorgobiani97@gmail.com>
2022-09-18 21:52:45 +0400

    Internationalizing Dates pt2

--- script.js
+++ script.js
@@ -185,23 +185,35 @@ updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
 //Experimenting API
+
+// const locale = navigator.language;
+// console.log(locale);
+
+// Create currnet date and time
+
 const now = new Date();
 const options = {
   hour: 'numeric',
   minute: 'numeric',
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+  weekday: 'long',
 };
 
-labelDate.textContent = new Intl.DateTimeFormat('en-US', options).format(now);
+labelDate.textContent = new Intl.DateTimeFormat(
+  currentAccount.locale,
+  options
+).format(now);
 
-// Create currnet date and time
 // const now = new Date();
-const day = `${now.getDate()}`.padStart(2, 0);
+/* const day = `${now.getDate()}`.padStart(2, 0);
 const month = `${now.getMonth() + 1}`.padStart(2, 0);
 const year = now.getFullYear();
 const hour = `${now.getHours()}`.padStart(2, 0);
-const min = `${now.getMinutes()}`.padStart(2, 0);
+const min = `${now.getMinutes()}`.padStart(2, 0); */
 
-labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+// labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
 // Day/Month/Year
 
